93ef309d5173e51c5952be7f68018d79d66b298a
Syabil76 <142871006+Syabil76@users.noreply.github.com>
2024-02-26 14:21:39 +0800

    Update Integration.py

--- Integration.py
+++ Integration.py
@@ -1,14 +1,17 @@
 #to be paired with my functions code
 
 def integration():
-  # bounds = int(input("please input your boundaries"))
-  integrated = []
-  for i in range(degree+1, 0, -1):
-      print(f"{funny[degree+1-i]} to the power of {i} divided by {i}")
-      new = (
-          (((funny[degree+1-i])**i)/i)
-      )
-      integrated.append(new)
-  print(integrated)
+    bound = int(input("please input your boundaries"))
+    integrated = []
+    newpoly = []
+    for i in range(degree+1, 0, -1):
+        print(f"{funny[degree+1-i]} to the power of {i} divided by {i}")
+        new = round(
+            ((funny[degree+1-i])/i), 5
+        )
+        integrated.append(new)
+        newpoly.append(''.join(map(str, new))+"+")
+    print(f"indefinite integral = {''.join(map(str, newpoly))} + C")
+
 
 integration()
