a7bd38726d16808d99b480cb89cdbeabf24d7279
Syabil76 <142871006+Syabil76@users.noreply.github.com>
2024-08-12 15:50:26 +0800

    Create websocket.go

---
+++ FullStack/GoChatApp/Backend/pkg/websocket/websocket.go
@@ -0,0 +1,54 @@
+package websocket
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+func Upgrader(w http.ResponseWriter, r *http.Request) (*websocket.Conn, error) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return ws, err
+	}
+	return ws, nil
+}
+
+func Reader(conn *websocket.Conn) {
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		fmt.Println(string(p))
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
+func Writer(conn *websocket.Conn) {
+	for {
+		fmt.Println("sending...")
+		messageType, r, err := conn.NextReader()
+		if err != nil {
+			log.Println(err)
+			return 
+		}
+		w
+	}
+}
