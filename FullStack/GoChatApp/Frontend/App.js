692ccd5646d55eefda06151fd69a6f21fdf7b482
Syabil76 <142871006+Syabil76@users.noreply.github.com>
2024-08-13 14:39:11 +0800

    Create App.js

---
+++ FullStack/GoChatApp/Frontend/App.js
@@ -0,0 +1,44 @@
+// App.js
+
+import React, { Component } from "react";
+import "./App.css";
+import {connect, sendMsg} from "./api";
+import Header from "./Components/Header";
+import ChatHistory from "./Components/ChatHistory";
+
+class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      chatHistory: []
+    }
+    connect();
+  }
+
+  componentDidMount() {
+    connect((msg) => {
+      console.log("New Message")
+      this.setState(prevState => ({
+        chatHistory: [...this.state.chatHistory, msg]
+      }))
+      console.log(this.state);
+    });
+  }
+
+  send() {
+    console.log("hello");
+    sendMsg("hello");
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <Header />
+        <ChatHistory chatHistory={this.state.chatHistory} />
+        <button onClick={this.send}>Hit</button>
+      </div>
+    );
+  };
+}
+
+export default App;
