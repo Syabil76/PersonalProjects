78aefde1d5bbd3d3cd42b9e08f561deca4776e16
Syabil76 <142871006+Syabil76@users.noreply.github.com>
2024-08-15 14:31:15 +0800

    Update main.go

--- FullStack/GoChatApp/Backend/main.go
+++ FullStack/GoChatApp/Backend/main.go
@@ -2,64 +2,28 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 
-	"github.com/gorilla/websocket"
+	"github.com/TutorialEdge/realtime-chat-go-react/pkg/websocket"
 )
 
-//Checks origin of messages
-//Allows us to make requests to React
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-
-	CheckOrigin: func(r *http.Request) bool { return true },
-}
-
-// read messages sent by client and prints
-func readmessage(conn *websocket.Conn) {
-	for {
-		messageType, p, err := conn.ReadMessage()
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		fmt.Println(string(p))
-
-		if err := conn.WriteMessage(messageType, p); err != nil {
-			log.Println(err)
-			return
-		}
-
-	}
-}
-
-//Defining our endpoint
-//Also upgrades connection to websocket
 func serveWs(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.Host)
-
-	ws, err := upgrader.Upgrade(w, r, nil)
+	ws, err := websocket.Upgrader(w, r)
 	if err != nil {
-		log.Println(err)
-		return
+		fmt.Fprintf(w, "%+V\n", err)
 	}
-
-	readmessage(ws)
+	go websocket.Writer(ws)
+	websocket.Reader(ws)
 }
 
 func setuproutes() {
-	// Handles incoming requests and written responses and prints it out using function earlier
+	// Handles incoming requests and written responses and prints it out
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Chat server")
 	})
 
 	http.HandleFunc("/ws", serveWs)
 }
-
-//opens live server
 
 func main() {
 	fmt.Println("Chat server v0.01")
