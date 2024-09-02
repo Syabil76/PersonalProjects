e8cc4db8278127fcd2d1b8e551537d1bf1d381ef
Syabil76 <142871006+Syabil76@users.noreply.github.com>
2024-09-02 13:10:20 +0800

    Update main.py

--- Python/MonsterCode/main.py
+++ Python/MonsterCode/main.py
@@ -1,11 +1,13 @@
-
 monsters = dict()
-with open("index.txt", 'r') as file:
+stats = dict{
+    
+}
+with open("test.txt", 'r') as file:
     lines = file.readlines()
     for line in lines:
         data = line.strip().split(',')
-        monsters[data[1]] = [data[0], *data[2:]]
-print(monsters)
+        monsters[data[0]] = [data[1], *data[2:]]
 
 for key, value in monsters.items():
-    print(tabulate(key, value))
+    print(f"Monster: {key}\n{value}")
+
